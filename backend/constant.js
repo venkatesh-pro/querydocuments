22466a890202698fb21329d3f3b6dc0a7193bb97
venkatesh-pro <programervenkatesh@gmail.com>
2023-07-25 15:06:26 +0530

    changed to final plan feature

--- backend/constant.js
+++ backend/constant.js
@@ -1,25 +1,25 @@
 const planFeature = {
   free: {
-    maxFileUsedPerDay: 2,
-    maxQuestionsAskedPerDay: 3,
+    maxFileUsedPerDay: 3,
+    maxQuestionsAskedPerDay: 30,
     // maxFileUsed: 60,
     // maxQuestionsAsked: 600,
     maximumPage: 20,
   },
   basic: {
-    maxFileUsedPerDay: 4,
-    maxQuestionsAskedPerDay: 6,
+    maxFileUsedPerDay: 5,
+    maxQuestionsAskedPerDay: 100,
     // maxFileUsed: 100,
     // maxQuestionsAsked: 2000,
-    maximumPage: 100,
+    maximumPage: 300,
   },
   pro: {
-    maxFileUsedPerDay: 6,
-    maxLinkPerDay: 6,
-    maxQuestionsAskedPerDay: 9,
+    maxFileUsedPerDay: 20,
+    maxLinkPerDay: 20,
+    maxQuestionsAskedPerDay: 500,
     // maxFileUsed: 400,
     // maxQuestionsAsked: 10000,
-    maximumPage: 200,
+    maximumPage: 1500,
   },
 };
 
